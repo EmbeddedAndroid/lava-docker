7c8fbd86c7063000b0f1ee4a3a045d04dea9a7ad
Tyler Baker <tyler.baker@linaro.org>
2017-04-24 20:07:00 -0700

    scripts/setup.sh: setup the admin user's token
    Signed-off-by: Tyler Baker <tyler.baker@linaro.org>

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -6,10 +6,16 @@ fi
 # Create the kernel-ci user
 echo "from django.contrib.auth.models import User; User.objects.create_superuser('kernel-ci', 'admin@localhost.com', 'kernel-ci')" | lava-server manage shell
 # Set the kernel-ci user's API token
-if [[ -n "$LAVA_API_TOKEN" ]]; then
-	lava-server manage tokens add --user kernel-ci --secret $LAVA_API_TOKEN
+if [[ -n "$KCI_API_TOKEN" ]]; then
+	lava-server manage tokens add --user kernel-ci --secret $KCI_API_TOKEN
 else
 	lava-server manage tokens add --user kernel-ci
+fi
+# Set the admin token
+if [[ -n "$ADMIN_API_TOKEN" ]]; then
+	lava-server manage tokens add --user admin --secret $ADMIN_API_TOKEN
+else
+	lava-server manage tokens add --user admin
 fi
 # Add workers
 lava-server manage pipeline-worker --hostname lava-slave-01
@@ -26,11 +32,11 @@ lava-server manage add-device --device-type qemu --worker lava-slave-01 qemu-04
 lava-server manage device-dictionary --hostname qemu-04 --import /etc/dispatcher-config/devices/qemu.jinja2
 # Add IoT Devices
 lava-server manage device-types add nrf52-nitrogen
-lava-server manage add-device --device-type nrf52-nitrogen --worker lava-slave-02 nrf52-nitrogen-01
+lava-server manage add-device --device-type nrf52-nitrogen --worker lava-slave-01 nrf52-nitrogen-01
 lava-server manage device-dictionary --hostname nrf52-nitrogen-01 --import /etc/dispatcher-config/devices/nrf52-nitrogen-01.jinja2
 lava-server manage device-types add stm32-carbon
-lava-server manage add-device --device-type stm32-carbon --worker lava-slave-02 stm32-carbon-01
+lava-server manage add-device --device-type stm32-carbon --worker lava-slave-01 stm32-carbon-01
 lava-server manage device-dictionary --hostname stm32-carbon-01.jinja2 --import /etc/dispatcher-config/devices/stm32-carbon-01.jinja2
 lava-server manage device-types add frdm-k64f
-lava-server manage add-device --device-type frdm-k64f --worker lava-slave-02 frdm-k64f-01
+lava-server manage add-device --device-type frdm-k64f --worker lava-slave-01 frdm-k64f-01
 lava-server manage device-dictionary --hostname frdm-k64f-01 --import /etc/dispatcher-config/devices/frdm-k64f-01.jinja2
